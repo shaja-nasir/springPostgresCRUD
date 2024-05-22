89797795b7e0ffaaf940b5fd0344b94458ff6e06
Shaja Nasir <Shajanasir@gmail.com>
2024-05-22 12:18:19 +0300

    Initial Setup

---
+++ src/main/java/co/spring/qa/springpostgrescrud/SpringPostgresCrudApplication.java
@@ -0,0 +1,13 @@
+package co.spring.qa.springpostgrescrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostgresCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPostgresCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/spring/qa/springpostgrescrud/SpringPostgresCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package co.spring.qa.springpostgrescrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPostgresCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
